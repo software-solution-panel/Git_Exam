55d4183e7c0165ce87dd8c4b160907ed347d280c
chathumal <chathumaljayasingha@hotmail.coim>
2021-10-09 10:36:41 +0530

    create CustomerView.fxml and CustomerView.java

---
+++ src/main/java/lk/ijse/exam/git_exam/CustomerView.java
@@ -0,0 +1,21 @@
+package lk.ijse.exam.git_exam;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * @author Chathumal Jayasingha | [chathumaljayasingha@hotmail.com]
+ * @since - 10/9/21 | 2021-October-09[Saturday]
+ */
+public class CustomerView {
+    public TextField txtCustomerName;
+    public TextField txtCustomerId;
+    public TextField txtCustomerAddress;
+    public TextField txtCustomerContact;
+    public Button btnAddCustomer;
+
+    public void addCustomer(ActionEvent actionEvent) {
+
+    }
+}
