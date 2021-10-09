546cba96b2dd08024758c3ffe6b7a514b998238c
chathumal <chathumaljayasingha@hotmail.coim>
2021-10-09 09:49:37 +0530

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module lk.ijse.exam.git_exam {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+
+    opens lk.ijse.exam.git_exam to javafx.fxml;
+    exports lk.ijse.exam.git_exam;
+}
